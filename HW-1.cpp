d6b72a9d756064202abc4aa2b61bdafbb7778cad
poommintk-blip <poommin.tk@gmail.com>
2025-12-10 00:37:33 +0700

    HW-01 v2

--- HW-1.cpp
+++ HW-1.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-bool isLeap(int year) {
-    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
-}
-
 int main() {
     int d, m, y;
-    cin >> d >> m >> y;
 
-    if (isLeap(y)) {
+    cin >> d;   
+    cin >> m;   
+    cin >> y;  
+
+    int daysInMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0)) {
         daysInMonth[1] = 29;
-    } else {
-        daysInMonth[1] = 28;
     }
-    d++;
+
+    d++; 
+
     if (d > daysInMonth[m-1]) {
         d = 1;
         m++;
+
         if (m > 12) {
             m = 1;
             y++;
